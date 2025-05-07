383621585ecdb7aa858ac328f6591ef2dc176b6b
Tomar1907 <soumyatomar1908@gmail.com>
2025-05-08 00:26:12 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from './components/Navbar';
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/SignUpPage";
+import SettingPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/SignUpPage";
+import {Routes , Route} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+     <Navbar />
+
+     <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/signup" element={<SignUpPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/settings" element={<SettingPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+
+     </Routes>
+    </div>
+  );
+};
+
+export default App;
