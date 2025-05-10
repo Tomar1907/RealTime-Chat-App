669b49711d2621978da9c04f8e44ffa8ffdd5696
Tomar1907 <soumyatomar1908@gmail.com>
2025-05-10 21:47:05 +0530

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-import { app , server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,7 +25,16 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, ()=>{
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
+server.listen(PORT, () => {
     console.log("server is running on port :" + PORT);
     connectDB();
 })
